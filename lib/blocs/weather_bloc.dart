9673123027528a52696247930277fbfb3b539da5
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-07-01 17:41:52 +0700

    implement WeatherBloc

--- lib/blocs/weather_bloc.dart
+++ lib/blocs/weather_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter_bloc_weather/models/models.dart';
+import 'package:flutter_bloc_weather/repositories/repositories.dart';
 import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
@@ -9,13 +10,24 @@ part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc({@required this.weatherRepository})
+      : assert(weatherRepository != null);
+
   @override
   WeatherState get initialState => WeatherInitial();
 
   @override
-  Stream<WeatherState> mapEventToState(
-    WeatherEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
+  Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
+    if (event is WeatherRequested) {
+      yield WeatherLoadInProgress();
+      try {
+        final Weather weather = await weatherRepository.getWeather(event.city);
+        yield WeatherLoadSuccess(weather: weather);
+      } catch (_) {
+        yield WeatherLoadFailure();
+      }
+    }
   }
 }
