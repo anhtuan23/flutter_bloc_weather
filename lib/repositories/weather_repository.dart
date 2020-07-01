b217115a62edcdbc7130705e1a389b0f7c7aa398
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-07-01 17:17:02 +0700

    add weather_repository

---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter_bloc_weather/models/models.dart';
+import 'package:flutter_bloc_weather/repositories/weather_api_client.dart';
+import 'package:meta/meta.dart';
+
+class WeatherRepository {
+  final WeatherApiClient weatherApiClient;
+  WeatherRepository({@required this.weatherApiClient})
+      : assert(weatherApiClient != null);
+
+  Future<Weather> getWeather(String city) async {
+    final int locationId = await weatherApiClient.getLocationId(city);
+    return weatherApiClient.fetchWeather(locationId);
+  }
+}
