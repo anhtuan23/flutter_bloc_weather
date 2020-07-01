ffaf56da3304dfd1425e7157c301e87dbd26a635
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-07-01 16:56:50 +0700

    add weather_api_client

---
+++ lib/repositories/weather_api_client.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherApiClient {
+  static const baseUrl = 'https://www.metaweather.com';
+  final http.Client httpClient;
+
+  WeatherApiClient({
+    @required this.httpClient,
+  }) : assert(httpClient != null);
+
+  Future<int> getLocationId(String city) async {
+    final locationUrl = '$baseUrl/api/location/search/?query=$city';
+    final locationResponse = await this.httpClient.get(locationUrl);
+    if (locationResponse.statusCode != 200) {
+      throw Exception('error getting locationId for city');
+    }
+
+    final locationJson = jsonDecode(locationResponse.body) as List;
+    return (locationJson.first)['woeid'];
+  }
+}
